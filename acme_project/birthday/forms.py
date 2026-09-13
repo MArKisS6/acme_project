af93080edfd2bdce30ba8c8a6b3c3e33065655d2
MArKisS6 <playmilokin@gmail.com>
2026-09-14 00:49:16 +0300

    ц
    ц

--- acme_project/birthday/forms.py
+++ acme_project/birthday/forms.py
@@ -1,16 +1,15 @@
 from django import forms
 
+from .models import Birthday
+
 MAX_L = 20
 
 
-class BirthdayForm(forms.Form):
-    first_name = forms.CharField(label='Имя', max_length=MAX_L)
-    last_name = forms.CharField(
-        required=False,
-        label="Фамилия",
-        help_text='Необязательное поле'
-    )
-    birthday = forms.DateField(
-        label='Дата рождения',
-        widget=forms.DateInput(attrs={'type': 'date'})
-    )
+class BirthdayForm(forms.ModelForm):
+
+    class Meta:
+        model = Birthday
+        fields = '__all__'
+        widgets = {
+            'birthday': forms.DateInput(attrs={'type': 'date'})
+        }
