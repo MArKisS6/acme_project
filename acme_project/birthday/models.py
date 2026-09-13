7cf2bba0fa950ffe7a803ab04f8fefe79c1ff749
MArKisS6 <playmilokin@gmail.com>
2026-09-13 23:14:36 +0300

    w
    w

--- acme_project/birthday/models.py
+++ acme_project/birthday/models.py
@@ -1,1 +1,14 @@
-# from django.db import models
+from django.db import models
+
+MAX_L = 20
+
+
+class Birthday(models.Model):
+    first_name = models.CharField('Имя', max_length=MAX_L)
+    last_name = models.CharField(
+        'Фамилия',
+        blank=True,
+        help_text='Необязательное поле',
+        max_length=MAX_L
+    )
+    birthday = models.DateField('Дата рождения')
