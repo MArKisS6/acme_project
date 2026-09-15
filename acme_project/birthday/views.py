93dc7f9bca5bd1422a6b89c0efb32e83ccbde5fa
MArKisS6 <playmilokin@gmail.com>
2026-09-15 09:50:34 +0300

    Update views.py

--- acme_project/birthday/views.py
+++ acme_project/birthday/views.py
@@ -20,3 +20,10 @@ def birthday(request):
         # Обновляем словарь контекста: добавляем в него новый элемент.
         context.update({'birthday_countdown': birthday_countdown})
     return render(request, 'birthday/birthday.html', context)
+
+def birthday_list(request):
+    # Получаем все объекты модели Birthday из БД.
+    birthdays = Birthday.objects.all()
+    # Передаём их в контекст шаблона.
+    context = {'birthdays': birthdays}
+    return render(request, 'birthday/birthday_list.html', context) 
