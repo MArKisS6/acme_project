0f9c2b124eb63c1d729bf1c7e9c8897439eb0d16
MArKisS6 <playmilokin@gmail.com>
2026-09-21 00:18:30 +0300

    ц
    ц

--- acme_project/users/models.py
+++ acme_project/users/models.py
@@ -1,6 +1,14 @@
-from django.contrib.auth.models import AbstractUser
-from django.db import models
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
+from .models import MyUser
 
-class MyUser(AbstractUser):
-    bio = models.TextField('Биография', blank=True)
+# Добавляем поле с биографией
+# к стандартному набору полей (fieldsets) пользователя в админке.
+UserAdmin.fieldsets += (
+    # Добавляем кортеж, где первый элемент — это название раздела в админке,
+    # а второй элемент — словарь, где ключом fields указать нужные поля.
+    ('Extra Fields', {'fields': ('bio',)}),
+)
+# Регистрируем модель в админке:
+admin.site.register(MyUser, UserAdmin)
